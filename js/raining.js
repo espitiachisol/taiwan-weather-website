5e4323bc0aa51d95505525d6197cec40ce59fdce
Sol Chi <espitiachisol@gmail.com>
2021-06-07 21:43:16 +0800

    base file

---
+++ js/raining.js
@@ -0,0 +1,26 @@
+let records=null;
+fetch("https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization="+CWB_API_KEY).then((response)=>{
+	return response.json();
+}).then((data)=>{
+	records=data.records;
+	renderRaining(0);
+});
+function renderRaining(page){
+	let startIndex=page*10;
+	let endIndex=(page+1)*10;
+	const container=document.querySelector("#raining");
+	for(let i=startIndex;i<endIndex;i++){
+		const location=records.location[i];
+		const item=document.createElement("div");
+		item.className="location";
+		const town=document.createElement("div");
+		town.className="town";
+		town.textContent=location.parameter[0].parameterValue+"、"+location.parameter[2].parameterValue;
+		const amount=document.createElement("amount");
+		amount.className="amount";
+		amount.textContent=location.weatherElement[6].elementValue+" mm";
+		item.appendChild(town);
+		item.appendChild(amount);
+		container.appendChild(item);
+	}
+}
